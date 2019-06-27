ce7f9fae80595d45aab7b9f4c750c3c9c930f1a1
EricRissler <eric.rissler@gmx.de>
2019-06-27 10:11:22 +0200

    Fixes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,13 +8,50 @@ declare var require: any;
 export class RegisterComponent implements OnInit {
   private Billy = require("../../assets/img/Billy.png");
 
-  step: number = 1;
+  penis: boolean = false;
 
+  land: string = "";
+  firstname: string = "";
+  lastname: string = "";
+  mail: string = "";
+  password: string = "";
+  passwordagain: string = "";
+  nationality: string = "";
+  streetandnumber: string = "";
+  adressplus: string = "";
+  postcode: string = "";
+  place: string = "";
+  birth: string = "";
+
+  step: number = 1;
   constructor() {}
+
+  Values() {
+    console.log("Land: " + this.land);
+    console.log("firstname: " + this.firstname);
+    console.log("lastname: " + this.lastname);
+    console.log("mail: " + this.mail);
+    console.log("password: " + this.password);
+    console.log("passwordagain: " + this.passwordagain);
+    console.log("nationality: " + this.nationality);
+    console.log("streetandnumber: " + this.streetandnumber);
+    console.log("adressplus:" + this.adressplus);
+    console.log("postcode: " + this.postcode);
+    console.log("Place: " + this.place);
+    console.log("Birth:" + this.birth);
+  }
 
   changestep() {
     this.step = this.step + 1;
     console.log(this.step);
+  }
+
+  disablebutton(penis) {
+    if (this.land == "") {
+    } else {
+      this.penis = true;
+    }
+    return this.penis;
   }
 
   ngOnInit() {}
