1340064660be7507342c39d3132a5b56c9783632
SebastianMichaelis1997 <sebastian@sileachim.de>
2019-06-28 22:21:51 +0200

    remove testing console logs

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -3,47 +3,44 @@ const adress = require("../sequelize").adress;
 const bcrypt = require("bcrypt");
 
 //FIXME:
-const getUser = function (req, res) {
+const getUser = function(req, res) {
   //authentification is very simple
   const data = {
     email: req.body.email,
     password: req.body.password
   };
-  privateUser.findOne({
-    where: {
-      email: data.email
-    }
-  }).then((user) => {
-    if (user == null) {
-      res.status(404).json({
-        message: "Invalid Userdata"
-      });
-      return;
-    }
-    if (user.password != null && user.password == data.password) {
-      console.log("Entered correct data part");
-      console.log("Succesfully parsed object");
-      res.status(200).json({
-        id: user.id,
-        firstname: user.firstname,
-        lastname: user.lastname,
-        birthdate: user.birthdate,
-        nationality: user.nationality,
-        email: user.email,
-
-      });
-    } else {
-      res.status(404).json({
-        message: "Invalid Userdata"
-      })
-    }
-  });
-
-}
-
+  privateUser
+    .findOne({
+      where: {
+        email: data.email
+      }
+    })
+    .then(user => {
+      if (user == null) {
+        res.status(404).json({
+          message: "Invalid Userdata"
+        });
+        return;
+      }
+      if (user.password != null && user.password == data.password) {
+        res.status(200).json({
+          id: user.id,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          birthdate: user.birthdate,
+          nationality: user.nationality,
+          email: user.email
+        });
+      } else {
+        res.status(404).json({
+          message: "Invalid Userdata"
+        });
+      }
+    });
+};
 
 //TODO: Bcrypt hash einfügen
-const postUser = function (req, res) {
+const postUser = function(req, res) {
   const SALTROUNDS = 12;
   const data = {
     country: req.body.country,
@@ -67,56 +64,62 @@ const postUser = function (req, res) {
         continue;
       }
       res.status(422).json({
-        "message": "value in field" + key + "is missing"
+        message: "value in field" + key + "is missing"
       });
     }
   }
 
-  privateUser.findOne({
-    where: {
-      email: data.email
-    }
-  }).then((user) => {
-    if (user != null) {
-      res.status(409).json({
-        "message": "email already registered"
-      });
-    } else {
-      adress.create({
-        strHouseNr: data.strHouseNr,
-        zipCode: data.zip,
-        city: data.city,
-        country: data.country,
-        additonal: data.additonal
-      }).then(function (result) {
-        console.log("Created Adress");
-        privateUser.create({
-          firstname: data.firstname,
-          lastname: data.lastname,
-          birthdate: data.bdate,
-          nationality: data.nationality,
-          email: data.email,
-          password: data.password,
-          idAdress: result.id
-        }).then(function (result) {
-          console.log("Created User");
-          res.status(201).json("Contact created");
-        })
-      }).catch(err => {
-        res.status(501).json(err);
-        console.log(err);
-      })
-    }
-  });
-}
+  privateUser
+    .findOne({
+      where: {
+        email: data.email
+      }
+    })
+    .then(user => {
+      if (user != null) {
+        res.status(409).json({
+          message: "email already registered"
+        });
+      } else {
+        adress
+          .create({
+            strHouseNr: data.strHouseNr,
+            zipCode: data.zip,
+            city: data.city,
+            country: data.country,
+            additonal: data.additonal
+          })
+          .then(function(result) {
+            console.log("Created Adress");
+            privateUser
+              .create({
+                firstname: data.firstname,
+                lastname: data.lastname,
+                birthdate: data.bdate,
+                nationality: data.nationality,
+                email: data.email,
+                password: data.password,
+                idAdress: result.id
+              })
+              .then(function(result) {
+                console.log("Created User");
+                res.status(201).json("Contact created");
+              });
+          })
+          .catch(err => {
+            res.status(501).json(err);
+            console.log(err);
+          });
+      }
+    });
+};
 
 module.exports = {
   get: getUser,
   post: postUser
-}
+};
 
-
-exports.module = function (req, res) {
+exports.module = function(req, res) {
   contact.get((req, res) => {
     if (err) {
       res.json({
