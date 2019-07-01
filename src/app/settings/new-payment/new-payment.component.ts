f0b753a65577ec355f1ba07fca44daef2686400c
EricRissler <eric.rissler@gmx.de>
2019-07-01 12:30:29 +0200

    Settings

--- src/app/settings/new-payment/new-payment.component.ts
+++ src/app/settings/new-payment/new-payment.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-new-payment',
-  templateUrl: './new-payment.component.html',
-  styleUrls: ['./new-payment.component.css']
+  selector: "app-new-payment",
+  templateUrl: "./new-payment.component.html",
+  styleUrls: ["./new-payment.component.css"]
 })
 export class NewPaymentComponent implements OnInit {
+  newBank: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  newCard: boolean = false;
+  newBankPayment() {
+    this.newBank = !this.newBank;
   }
 
+  newKreditcard() {
+    this.newCard = !this.newCard;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
