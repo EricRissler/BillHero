7cad13aa2c1faa932b30d039ebe243db98d0dfa4
DominikVolz <40237926+DominikVolz@users.noreply.github.com>
2019-07-17 15:44:17 +0200

    Bills

--- src/app/bill.service.ts
+++ src/app/bill.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PrvUserServiceService } from './prv-user-service.service';
 import { Bill } from './shared/bill.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -17,37 +18,51 @@ export class BillService {
   deadline: String;
   categoryID: String;
   paymentId: String;
-  shortname:String;
-  bills:Bill[];
-  bill:Bill[];
+  shortname: String;
+  bills: Bill[];
+  bill: Bill[];
+  b: Bill;
   uid: String;
-  constructor(private http: HttpClient, private prvUserService: PrvUserServiceService) {
-   // this.uid = prvUserService.getUID();
+
+  constructor(private http: HttpClient, private prvUserService: PrvUserServiceService,
+    private router: Router) {
+
   }
 
-
+  setUID(uid) {
+    this.uid = uid;
+  }
 
   getAllBills() {
-    this.uid="117248c0-a295-11e9";
+    //this.uid="3ff9d230-a7ef-11e9";
+    // this.uid = uid;
+    this.uid = this.prvUserService.getUID();
+    //alert(this.uid);
+    //  const uid2 = this.uid;
     this.http
-      .get<{bills:Bill[]}>("http://localhost:3000/api/prvusers/" + this.uid + "/bills")
+      .get<{ bills: Bill[] }>("http://localhost:3000/api/prvusers/" + this.uid + "/bills")
+      // .get<{ bills: Bill[] }>("http://localhost:3000/api/prvusers/3ff9d230-a7ef-11e9/bills")
       .subscribe(responseData => {
         //alert(responseData.bills);
-      // responseData.forEach(element => {
-       //  this.bills=responseData.bills;
-      // });
-       // this.billID = responseData.billID;
-       this.bills=responseData.bills;
-       console.log(this.bills[0]);
-       
-     // console.log(responseData.bill[0]);
-     // console.log(responseData.bill[1]);
+        // responseData.forEach(element => {
+        //  this.bills=responseData.bills;
+        // });
+        // this.billID = responseData.billID;
+        this.bills = responseData.bills;
+
+        //console.log(responseData.bills[0].payStatus);
+        // console.log(responseData.bill[1]);
       });
-       this.bills.forEach(element => {
-         alert(element.name); //j
-       //this.bill.push(new Bill("Media", "05.08.2019", 900, false));
-       });
-      return this.bills;
+
+    // this.bills.forEach(element => {
+    //alert(element.shortname+" "+ element.deadline +" "+element.amount);
+    // new Bill(element.shortname, element.deadline, element.amount, false)
+    //this.bill.push(this.b);
+    //  });
+    //  this.bill.forEach(element => {
+    //    alert(element.shortname);
+    //  });
+    return this.bills;
   }
 
 }
--- src/app/mybills/mybills.component.ts
+++ src/app/mybills/mybills.component.ts
@@ -23,7 +23,9 @@ export class MybillsComponent implements OnInit {
     private router: Router) { }
   public input: string = "";
 
+  uid:String;
    bill: Bill[] ;
+   bills: Bill[];
    //= [
   //   new Bill("Media Markt", "05.08.2019", 900, false),
   //   new Bill("Schreiner", "14.06.2019", 750, false),
@@ -51,31 +53,50 @@ export class MybillsComponent implements OnInit {
 
     if(!this.prvUserService.getUser()){
       this.router.navigate(["/signin"]);
-    }
-
+    }else{
+    this.uid= this.prvUserService.getUID();
+    
     this.headerService.setHeader(true);
-
+    
+    //this.billService.setUID(this.uid); 
     this.bill=this.billService.getAllBills();
-
+    if (this.bill==null) {
+      this.http
+      .get<{ bills: Bill[] }>("http://localhost:3000/api/prvusers/" + this.uid + "/bills")
+      .subscribe(responseData => {   
+        this.bill = responseData.bills;
+      });
+    }
+    
+// try {
+//   this.bill.forEach(element => {
+    
+//     this.bills.push(new Bill(element.shortname, element.deadline, element.amount, false));
+//     });
+// } catch (error) {
+  
+// }
+   
+    
     //  this.http.get<{bill:Bill[]}>('http://localhost:3000/api/mybills')
     //  .subscribe((billData) => {
        
     //    this.bill=billData.bill;
-       
+
     //  });
 
-      // this.bill= [
-      //    new Bill("Media Markt", "05.08.2019", "900", false),
-      //    new Bill("Schreiner", "14.06.2019", "750", false),
-      //    new Bill("Zahnarzt", "24.12.2019", "750", false),
-      //    new Bill("MEWA", "01.01.2020", "750", false),
-      //    new Bill("Media Markt", "05.08.2019", "900", true),
-      //    new Bill("Schreiner", "14.06.2019", "750", true),
-      //    new Bill("Media Markt", "05.08.2019", "900", true),
-      //    new Bill("Schreiner", "14.06.2019", "750", true),
-      //    new Bill("Zahnarzt", "24.12.2019", "750", true),
+      //  this.bill= [
+      //     new Bill("Media Markt", "05.08.2019", "900", false),
+      //     new Bill("Schreiner", "14.06.2019", "750", false),
+      //     new Bill("Zahnarzt", "24.12.2019", "750", false),
+      //     new Bill("MEWA", "01.01.2020", "750", false),
+      //     new Bill("Media Markt", "05.08.2019", "900", true),
+      //     new Bill("Schreiner", "14.06.2019", "750", true),
+      //     new Bill("Media Markt", "05.08.2019", "900", true),
+      //     new Bill("Schreiner", "14.06.2019", "750", true),
+      //     new Bill("Zahnarzt", "24.12.2019", "750", true),
          
-      //    ];
-
+      //     ];
+    }
   }
 }
